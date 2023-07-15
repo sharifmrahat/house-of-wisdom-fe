fb7bdf273583f2af554c661570327774d9b4d3ff
sharifmrahat <sharifmrahat@gmail.com>
2023-07-15 20:47:45 +0600

    shadcn setup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import { Button } from "@/components/ui/button";
 
 function App() {
   return (
     <>
       <p className="text-3xl font-bold underline text-red-500">Hello world!</p>
+      <div>
+        <Button>Click me</Button>
+      </div>
     </>
   );
 }
